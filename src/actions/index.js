f32b319f8f17b9afff6bc0fd9903907642dcb907
londonbaker4@gmail.com <londonbaker4@gmail.com>
2017-10-18 11:04:28 -0400

    working on ajax request

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY =	'a4130c640f989491c3149a1d93b3bc88';
+
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// Action creator to fetch weather
+export function fetchWeather(city) {
+
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
